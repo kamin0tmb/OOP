a4e807f5c1de0c235440aaa8d18259f5aa8fa087
kamin0 <jm187@yandex.ru>
2022-04-23 13:19:25 +0300

    6.2.2

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,11 +2,42 @@
 
 namespace OOP
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Pen
+	{
+		// Поля класса
+		public string color;
+		public int cost;
+
+		// Метод класса
+		public void PenCost()
+		{
+			Console.WriteLine("В наличии есть ручка цвета: {0}, стоимостью: {1} рублей", color, cost);
+		}
+		// Конструктор 2
+		public Pen()
+		{
+			color = "Черный";
+			cost = 100;
+		}
+		// Конструктор 3
+		public Pen(string penColor, int penCost)
+		{
+			color = penColor;
+			cost = penCost;
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pen pen = new Pen();
+			pen.PenCost();
+
+			pen = new Pen("Красный", 150);
+			pen.PenCost();
+
+			
+		}
+	}
 }
