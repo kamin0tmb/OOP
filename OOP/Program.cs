3dcd2d6a758ebdcd5b76ce30c75fea8ed315e050
kamin0 <jm187@yandex.ru>
2022-04-30 18:34:58 +0300

    7.3.3

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,57 +2,39 @@
 
 namespace OOP
 {
-	class A
-	{
-		public virtual void Display()
-		{
-			Console.WriteLine("A");
-		}
-	}
-	class B : A
-	{
-		public new void Display()
-		{
-			Console.WriteLine("B");
-		}
-	}
-	class C : A
-	{
-		public override void Display()
-		{
-			Console.WriteLine("C");
-		}
-	}
-	class D : B
-	{
-		public new void Display()
-		{
-			Console.WriteLine("D");
-		}
-	}
-	class E : C
-	{
-		public new void Display()
-		{
-			Console.WriteLine("E");
-		}
-	}
-	class Program
-	{
-		
-			static void Main(string[] args)
-			{
-			D d = new D();
-			E e = new E();
+    
+    abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+    class Processor : ComputerPart
+    {
+        public override void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class MotherBoard : ComputerPart
+    {
+        public override void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class GraficCard : ComputerPart
+    {
+        public override void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-			d.Display();
-			((A)e).Display();
-			((B)d).Display();
-			((A)d).Display();
+        }
 
-
-		}
-		
-
-	}
+    }
 }
