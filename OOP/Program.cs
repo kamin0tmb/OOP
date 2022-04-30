04fabbd9a751b07cf87291d6c6ef29efbe8ff322
kamin0 <jm187@yandex.ru>
2022-04-30 18:58:34 +0300

    7.5.3

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -3,37 +3,28 @@
 namespace OOP
 {
     
-    abstract class ComputerPart
+    class Helper
     {
-        public abstract void Work();
-    }
-    class Processor : ComputerPart
-    {
-        public override void Work()
+        public static void Swap(ref int num1, ref int num2)
         {
-            throw new NotImplementedException();
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
     }
-    class MotherBoard : ComputerPart
-    {
-        public override void Work()
-        {
-            throw new NotImplementedException();
-        }
-    }
-    class GraficCard : ComputerPart
-    {
-        public override void Work()
-        {
-            throw new NotImplementedException();
-        }
-    }
+    
     
     class Program
     {
         static void Main(string[] args)
         {
+            int num1 = 3;
+            int num2 = 58;
 
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
         }
 
     }
