3499eb11dca95bc22597f04b55ce1356d104bc0a
kamin0 <jm187@yandex.ru>
2022-05-03 21:46:52 +0300

    7.7

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -2,42 +2,119 @@
 
 namespace OOP
 {
-
-    static class IntExtensions
-    {
-        public static int GetNegative(this int source)
-        {
-            if (source > 0)
-                return -source;
-            else return source;
-        }
-
-
-
-        public static int GetPositive(this int source)
-        {
-            if (source < 0)
-                return -source;
-            else return source;
-        }
-
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
+            NoteBook noteBook = new NoteBook();
+            Console.WriteLine("Характеристики ноутбука: {0}", noteBook.NameProduct());
 
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0 
+            var caseNoteBook = new CaseNoteBook();
+            var caseShop = caseNoteBook.noteBook.model;
+            Console.WriteLine("Наименование сумки для ноутбука: {0}", caseNoteBook.ChangeCase(caseShop));
+
+
+        }
+    }
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    abstract class Product
+    {
+        public string manufacturer;
+        public string model;
+        public int year;
+
+        public virtual string NameProduct() 
+        {
+            return "производитель:" + manufacturer + "модель:" + model + "год выпуска:" + year;
+        }
+        
+    }
+
+    class NoteBook : Product
+    {
+        public int cores; 
+        protected string diagonal; 
+
+        public NoteBook() 
+        {
+            manufacturer = "HP";
+            model = "250 G7";
+            year = 2022;
+            cores = 2;
+            diagonal = "15,6";
+        }
+        public int Cores
+        {
+            get
+            {
+                return cores;
+            }
+            set
+            {
+                if (value == 2 || value == 4 || value == 6 || value == 8)
+                {
+                    Console.WriteLine("Количество ядер может быть только 2, 4, 6 или 8");
+                }
+                else cores = value;
+            }
         }
 
+        public override string NameProduct()
+        {
+            return "производитель: " + manufacturer + " модель: " + model + " год выпуска: " + year + " количество ядер процессора: " + cores + " диагональ дисплея: " + diagonal;
+        }
+    }
+
+    class CaseNoteBook
+    {
+        public NoteBook noteBook;
+        public CaseNoteBook()
+        {
+            noteBook = new NoteBook();
+        }
+        public string ChangeCase(string model)
+        {
+            switch (noteBook.model)
+            {
+                case "250 G7":
+                    return "case156";
+                case "245 G8":
+                    return "case14";
+                case "elite dragonfly g2":
+                    return "case133";
+            }
+            return "Сумка для данной модели отсутствует в продаже";
+        }
+    }
+
+
+
+    class Order<TDelivery, TNoteBook, TCaseNoteBook>
+        where TDelivery : Delivery
+        where TNoteBook : NoteBook
+        where TCaseNoteBook : CaseNoteBook
+    {
+        public TDelivery Delivery;
+        public TNoteBook NoteBook;
+        public TCaseNoteBook CaseNoteBook;
+
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress(TDelivery Delivery)
+        {
+            Console.WriteLine("Адрес доставки" + Delivery);
+        }
+
+        public void DisplayProduct()
+        {
+            Console.WriteLine("Товар: " + NoteBook.NameProduct());
+        }
     }
 }
